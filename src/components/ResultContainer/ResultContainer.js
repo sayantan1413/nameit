092b4bbb711d89f688f746395d4b1fc797362481
sayantan1413 <sayantanbose2001@gmail.com>
2021-10-05 02:14:47 +0530

    Minor change
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

--- src/components/ResultContainer/ResultContainer.js
+++ src/components/ResultContainer/ResultContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ResultContainer.css'
-import NameCard from './../NameCard/NameCard'
+import NameCard from '../NameCard/NameCard'
 const ResultContainer = ({suggestedNames}) => {
     const suggestedNameJsx = suggestedNames.map(eachSuggestedName => {
         return <NameCard key={eachSuggestedName} eachSuggestedName={eachSuggestedName}/>
